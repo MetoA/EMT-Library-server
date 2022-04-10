1314573ec2aa68ec4635901b93f3e17e75573ed6
MetoA <metoandov@gmail.com>
2022-04-10 13:13:32 +0200

    Initial

---
+++ src/main/kotlin/com/metoa/emtlibrary/EmtLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.metoa.emtlibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmtLibraryApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmtLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/metoa/emtlibrary/EmtLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.metoa.emtlibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmtLibraryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
